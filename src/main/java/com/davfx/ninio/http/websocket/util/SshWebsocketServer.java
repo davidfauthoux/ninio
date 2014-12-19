476b64174bcd3b515b5d4ca5399334a57de01294
David Fauthoux <david.fauthoux@gmail.com>
2014-12-19 18:01:55 +0100

    Websocket->SSH

--- src/main/java/com/davfx/ninio/http/websocket/util/SshWebsocketServer.java
+++ src/main/java/com/davfx/ninio/http/websocket/util/SshWebsocketServer.java
@@ -13,8 +13,6 @@ import com.davfx.ninio.common.CloseableByteBufferHandler;
 import com.davfx.ninio.common.FailableCloseableByteBufferHandler;
 import com.davfx.ninio.common.Queue;
 import com.davfx.ninio.common.ReadyConnection;
-import com.davfx.ninio.common.ReadyFactory;
-import com.davfx.ninio.common.SocketReadyFactory;
 import com.davfx.ninio.http.HttpServer;
 import com.davfx.ninio.http.HttpServerConfigurator;
 import com.davfx.ninio.http.HttpServerHandler;
@@ -24,17 +22,29 @@ import com.davfx.ninio.http.util.PatternDispatchHttpServerHandler;
 import com.davfx.ninio.http.websocket.WebsocketHttpServerHandler;
 import com.davfx.ninio.ssh.SshClient;
 import com.davfx.ninio.ssh.SshClientConfigurator;
-import com.davfx.ninio.telnet.TelnetClientConfigurator;
+import com.davfx.util.ConfigUtils;
 import com.google.common.base.Charsets;
+import com.typesafe.config.Config;
 
 public final class SshWebsocketServer {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(SshWebsocketServer.class);
 	
-	public SshWebsocketServer(int webPort, final ReadyFactory readyFactory, final Address telnetAddress) throws IOException {
-		final Queue queue = new Queue();
-		final SshClient client = new SshClient(new SshClientConfigurator(queue).withAddress(telnetAddress).withLogin("dummy").withPassword("dummy"));
-		new HttpServer(new HttpServerConfigurator(queue).withPort(webPort), new HttpServerHandlerFactory() {
+	private static final Config CONFIG = ConfigUtils.load(SshWebsocketServer.class);
+
+	public static void main(String[] args) throws Exception {
+		Queue queue = new Queue();
+		new SshWebsocketServer(
+			new HttpServerConfigurator(queue).withPort(CONFIG.getInt("http.sshwebsocket.web.port")),
+			new SshClientConfigurator(queue).withAddress(new Address(CONFIG.getString("http.sshwebsocket.ssh.host"), CONFIG.getInt("http.sshwebsocket.ssh.port"))).withLogin(CONFIG.getString("http.sshwebsocket.ssh.login")).withPassword(CONFIG.getString("http.sshwebsocket.ssh.password")),
+			CONFIG.getString("http.sshwebsocket.path")
+		);
+	}
+	
+	
+	public SshWebsocketServer(HttpServerConfigurator httpConfigurator, SshClientConfigurator sshConfigurator, final String websocketPath) throws IOException {
+		final SshClient client = new SshClient(sshConfigurator);
+		new HttpServer(httpConfigurator, new HttpServerHandlerFactory() {
 			@Override
 			public void failed(IOException e) {
 			}
@@ -45,72 +55,67 @@ public final class SshWebsocketServer {
 			@Override
 			public HttpServerHandler create() {
 				return new PatternDispatchHttpServerHandler()
-				.add(Pattern.compile(".*\\.html"), new JsonDirectoryHttpServerHandler(new File(".")))
-				.add(Pattern.compile(".*"), new WebsocketHttpServerHandler(new ReadyConnection() {
-					@Override
-					public void failed(IOException e) {
-						LOGGER.debug("Failed from web", e);
-						if (innerTelnetWrite != null) {
-							innerTelnetWrite.failed(e);
+					.add(Pattern.compile(websocketPath), new WebsocketHttpServerHandler(new ReadyConnection() {
+						@Override
+						public void failed(IOException e) {
+							LOGGER.debug("Failed from web", e);
+							if (innerSshWrite != null) {
+								innerSshWrite.failed(e);
+							}
 						}
-					}
-					
-					@Override
-					public void close() {
-						LOGGER.debug("Closed from web");
-						if (innerTelnetWrite != null) {
-							innerTelnetWrite.close();
+						
+						@Override
+						public void close() {
+							LOGGER.debug("Closed from web");
+							if (innerSshWrite != null) {
+								innerSshWrite.close();
+							}
 						}
-					}
-					
-					private CloseableByteBufferHandler innerWebWrite;
-					private FailableCloseableByteBufferHandler innerTelnetWrite = null;
-
-					@Override
-					public void handle(Address address, ByteBuffer fromWebBuffer) {
-						LOGGER.debug("Received from web {} bytes: {}", fromWebBuffer.remaining(), new String(fromWebBuffer.array(), fromWebBuffer.position(), fromWebBuffer.remaining(), Charsets.UTF_8));
-						if (innerTelnetWrite != null) {
-							LOGGER.debug("Sent to telnet");
-							innerTelnetWrite.handle(address, fromWebBuffer);
+						
+						private CloseableByteBufferHandler innerWebWrite;
+						private FailableCloseableByteBufferHandler innerSshWrite = null;
+	
+						@Override
+						public void handle(Address address, ByteBuffer fromWebBuffer) {
+							LOGGER.debug("Received from web {} bytes: {}", fromWebBuffer.remaining(), new String(fromWebBuffer.array(), fromWebBuffer.position(), fromWebBuffer.remaining(), Charsets.UTF_8));
+							if (innerSshWrite != null) {
+								innerSshWrite.handle(address, fromWebBuffer);
+							}
 						}
-					}
-					
-					@Override
-					public void connected(FailableCloseableByteBufferHandler webWrite) {
-						innerWebWrite = webWrite;
-
-						client.connect(new ReadyConnection() {
-							@Override
-							public void connected(FailableCloseableByteBufferHandler telnetWrite) {
-								LOGGER.debug("Telnet connected");
-								innerTelnetWrite = telnetWrite;
-							}
-							
-							@Override
-							public void handle(Address address, ByteBuffer fromTelnetBuffer) {
-								LOGGER.debug("Received from telnet {} bytes: {}", fromTelnetBuffer.remaining(), new String(fromTelnetBuffer.array(), fromTelnetBuffer.position(), fromTelnetBuffer.remaining(), Charsets.UTF_8));
-								innerWebWrite.handle(address, fromTelnetBuffer);
-							}
-							
-							@Override
-							public void close() {
-								LOGGER.debug("Closed from telnet");
-								innerWebWrite.close();
-							}
-							
-							@Override
-							public void failed(IOException e) {
-								LOGGER.debug("Failed from telnet", e);
-								innerWebWrite.close();
-							}
-						});
-					}
-				}));
+						
+						@Override
+						public void connected(FailableCloseableByteBufferHandler webWrite) {
+							innerWebWrite = webWrite;
+	
+							client.connect(new ReadyConnection() {
+								@Override
+								public void connected(FailableCloseableByteBufferHandler sshWrite) {
+									LOGGER.debug("Ssh connected");
+									innerSshWrite = sshWrite;
+								}
+								
+								@Override
+								public void handle(Address address, ByteBuffer fromSshBuffer) {
+									LOGGER.debug("Received from ssh {} bytes: {}", fromSshBuffer.remaining(), new String(fromSshBuffer.array(), fromSshBuffer.position(), fromSshBuffer.remaining(), Charsets.UTF_8));
+									innerWebWrite.handle(address, fromSshBuffer);
+								}
+								
+								@Override
+								public void close() {
+									LOGGER.debug("Closed from ssh");
+									innerWebWrite.close();
+								}
+								
+								@Override
+								public void failed(IOException e) {
+									LOGGER.debug("Failed from ssh", e);
+									innerWebWrite.close();
+								}
+							});
+						}
+					}))
+					.add(Pattern.compile(".*"), new JsonDirectoryHttpServerHandler(new File(".")));
 			}
 		});
-	}
-	
-	public static void main(String[] args) throws Exception {
-		new SshWebsocketServer(8080, new SocketReadyFactory(), new Address("w.davfx.com", SshClientConfigurator.DEFAULT_PORT));
 	}
 }
