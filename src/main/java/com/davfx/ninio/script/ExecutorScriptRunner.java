5714dbbfae04231d1737365190bd347da90ff49f
David Fauthoux <david.fauthoux@gmail.com>
2015-02-25 12:31:02 +0100

    json|script modes to optimize

--- src/main/java/com/davfx/ninio/script/ExecutorScriptRunner.java
+++ src/main/java/com/davfx/ninio/script/ExecutorScriptRunner.java
@@ -19,12 +19,30 @@ import com.davfx.util.ConfigUtils;
 import com.davfx.util.Mutable;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigException;
 
 public final class ExecutorScriptRunner implements ScriptRunner<JsonElement>, AutoCloseable {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ExecutorScriptRunner.class);
 	
-	public static final String CALL_FUNCTION_NAME = ConfigUtils.load(ExecutorScriptRunner.class).getString("script.functions.call");
-	public static final String UNICITY_PREFIX = ConfigUtils.load(ExecutorScriptRunner.class).getString("script.functions.unicity.prefix");
+	private static final Config CONFIG = ConfigUtils.load(ExecutorScriptRunner.class);
+	
+	private static final boolean USE_TO_STRING;
+	static {
+		String mode = CONFIG.getString("script.mode");
+		if (mode.equals("string")) {
+			USE_TO_STRING = true;
+			LOGGER.debug("Mode: string");
+		} else if (mode.equals("json")) {
+			USE_TO_STRING = false;
+			LOGGER.debug("Mode: json");
+		} else {
+			throw new ConfigException.BadValue("script.mode", "Invalid mode, only allowed: json|script");
+		}
+	}
+	
+	public static final String CALL_FUNCTION_NAME = CONFIG.getString("script.functions.call");
+	public static final String UNICITY_PREFIX = CONFIG.getString("script.functions.unicity.prefix");
 	
 	private final ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
 	private final ExecutorService executorService = Executors.newSingleThreadExecutor(new ClassThreadFactory(ExecutorScriptRunner.class));
@@ -39,7 +57,7 @@ public final class ExecutorScriptRunner implements ScriptRunner<JsonElement>, Au
 	}
 	
 	// Must be be public to be called from javascript
-	public static final class FromScript {
+	public static final class FromScriptUsingConvert {
 		private final ScriptEngine scriptEngine;
 		private final ExecutorService executorService;
 		private final Mutable<Long> nextUnicitySuffix;
@@ -47,7 +65,7 @@ public final class ExecutorScriptRunner implements ScriptRunner<JsonElement>, Au
 		private final AsyncScriptFunction<JsonElement> asyncFunction;
 		private final SyncScriptFunction<JsonElement> syncFunction;
 		
-		private FromScript(ScriptEngine scriptEngine, ExecutorService executorService, Mutable<Long> nextCallbackFunctionSuffix, Failable fail, AsyncScriptFunction<JsonElement> asyncFunction, SyncScriptFunction<JsonElement> syncFunction) {
+		private FromScriptUsingConvert(ScriptEngine scriptEngine, ExecutorService executorService, Mutable<Long> nextCallbackFunctionSuffix, Failable fail, AsyncScriptFunction<JsonElement> asyncFunction, SyncScriptFunction<JsonElement> syncFunction) {
 			this.scriptEngine = scriptEngine;
 			this.fail = fail;
 			this.executorService = executorService;
@@ -56,65 +74,13 @@ public final class ExecutorScriptRunner implements ScriptRunner<JsonElement>, Au
 			this.syncFunction = syncFunction;
 		}
 
-		/*
-		private JsonElement toJson(Object javascriptObject) {
-			if (javascriptObject == null) {
-				return null;
-			}
-
-			Bindings bindings = scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE);
-			
-			long suffix = nextUnicitySuffix.get();
-			String parameterVar = UNICITY_PREFIX + "parameter" + suffix;
-			nextUnicitySuffix.set(suffix + 1);
-			
-			bindings.put(parameterVar, javascriptObject);
-			String r;
-			try {
-				r = (String) scriptEngine.eval("JSON.stringify(" + parameterVar + ")");
-			} catch (Exception e) {
-				LOGGER.error("Script error", e);
-				r = null;
-			}
-			
-			bindings.remove(parameterVar);
-			if (r == null) {
-				return null;
-			}
-			return new JsonParser().parse(r);
-		}
-		private Object fromJson(JsonElement jsonObject) {
-			if (jsonObject == null) {
-				return null;
-			}
-			
-			Bindings bindings = scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE);
-
-			//%% long suffix = nextUnicitySuffix.get();
-			String returnVar = UNICITY_PREFIX + "return"; //%% + suffix;
-			//%% nextUnicitySuffix.set(suffix + 1);
-			
-			String s = jsonObject.toString();
-			Object r;
-			try {
-				r = scriptEngine.eval(returnVar + " = " + s + ";");
-			} catch (Exception e) {
-				LOGGER.error("Script error", e);
-				r = null;
-			}
-			bindings.remove(returnVar);
-			return r;
-		}
-		*/
-		
-		public String call(String fromScriptParameter, final Object callback) throws ScriptException {
-			JsonElement fromScriptParameterAsJson = new JsonParser().parse(fromScriptParameter); //%% toJson(fromScriptParameter);
+		public JsonElement call(JsonElement fromScriptParameterAsJson, final Object callback) throws ScriptException {
 			if (callback == null) {
 				JsonElement r = syncFunction.call(fromScriptParameterAsJson);
 				if (r == null) {
 					return null;
 				}
-				return r.toString();
+				return r;
 			}
 			asyncFunction.call(fromScriptParameterAsJson, new AsyncScriptFunction.Callback<JsonElement>() {
 				@Override
@@ -132,17 +98,13 @@ public final class ExecutorScriptRunner implements ScriptRunner<JsonElement>, Au
 							
 							bindings.put(callbackVar, callback);
 
-							/*
 							suffix = nextUnicitySuffix.get();
 							String parameterVar = UNICITY_PREFIX + "parameter" + suffix;
 							nextUnicitySuffix.set(suffix + 1);
 							
-							bindings.put(parameterVar, response.toString());
-							*/
+							bindings.put(parameterVar, response);
 
-							//%%%%%%% String callbackScript = callbackVar + "(JSON.parse(" + new JsonPrimitive(response.toString()).toString() + "));";
-							String callbackScript = callbackVar + "(" + response.toString() + ");";
-							// String callbackScript = callbackVar + "(JSON.parse(" + parameterVar + "));";
+							String callbackScript = callbackVar + "(" + UNICITY_PREFIX + "convertTo(" + parameterVar + "));";
 							try {
 								LOGGER.trace("Executing callback: {}", callbackScript);
 								scriptEngine.eval(callbackScript);
@@ -154,11 +116,11 @@ public final class ExecutorScriptRunner implements ScriptRunner<JsonElement>, Au
 							}
 							
 							bindings.remove(callbackVar);
-							// bindings.remove(parameterVar);
+							bindings.remove(parameterVar);
 						}
 					});
 				}
-				/*
+				/*%%%%%%%%%%%%%%%
 				@Override
 				public void close() {
 					executorService.execute(new Runnable() {
@@ -195,6 +157,67 @@ public final class ExecutorScriptRunner implements ScriptRunner<JsonElement>, Au
 		}
 	}
 	
+	public static final class FromScriptUsingToString {
+		private final ScriptEngine scriptEngine;
+		private final ExecutorService executorService;
+		private final Mutable<Long> nextUnicitySuffix;
+		private final Failable fail;
+		private final AsyncScriptFunction<JsonElement> asyncFunction;
+		private final SyncScriptFunction<JsonElement> syncFunction;
+		
+		private FromScriptUsingToString(ScriptEngine scriptEngine, ExecutorService executorService, Mutable<Long> nextCallbackFunctionSuffix, Failable fail, AsyncScriptFunction<JsonElement> asyncFunction, SyncScriptFunction<JsonElement> syncFunction) {
+			this.scriptEngine = scriptEngine;
+			this.fail = fail;
+			this.executorService = executorService;
+			this.nextUnicitySuffix = nextCallbackFunctionSuffix;
+			this.asyncFunction = asyncFunction;
+			this.syncFunction = syncFunction;
+		}
+
+		public String call(String fromScriptParameter, final Object callback) throws ScriptException {
+			JsonElement fromScriptParameterAsJson = new JsonParser().parse(fromScriptParameter);
+			if (callback == null) {
+				JsonElement r = syncFunction.call(fromScriptParameterAsJson);
+				if (r == null) {
+					return null;
+				}
+				return r.toString();
+			}
+			asyncFunction.call(fromScriptParameterAsJson, new AsyncScriptFunction.Callback<JsonElement>() {
+				@Override
+				public void handle(final JsonElement response) {
+					executorService.execute(new Runnable() {
+						@Override
+						public void run() {
+							Bindings bindings = scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE);
+							
+							long suffix;
+							
+							suffix = nextUnicitySuffix.get();
+							String callbackVar = UNICITY_PREFIX + "callback" + suffix;
+							nextUnicitySuffix.set(suffix + 1);
+							
+							bindings.put(callbackVar, callback);
+
+							String callbackScript = callbackVar + "(" + response.toString() + ");";
+							try {
+								LOGGER.trace("Executing callback: {}", callbackScript);
+								scriptEngine.eval(callbackScript);
+							} catch (Exception e) {
+								LOGGER.error("Script error in: {}", callbackScript, e);
+								if (fail != null) {
+									fail.failed(new IOException(e));
+								}
+							}
+							
+							bindings.remove(callbackVar);
+						}
+					});
+				}
+			});
+			return null;
+		}
+	}
 	@Override
 	public void eval(final Iterable<String> script, final Failable fail, final AsyncScriptFunction<JsonElement> asyncFunction, final SyncScriptFunction<JsonElement> syncFunction) {
 		executorService.execute(new Runnable() {
@@ -204,8 +227,73 @@ public final class ExecutorScriptRunner implements ScriptRunner<JsonElement>, Au
 				Bindings bindings = scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE);
 				
 				String callVar = UNICITY_PREFIX + "call";
-				bindings.put(callVar, new FromScript(scriptEngine, executorService, nextCallbackFunctionSuffix, fail, asyncFunction, syncFunction));
-				String callFunctions = "var " + CALL_FUNCTION_NAME + " = function(parameter, callback) { return JSON.parse(" + callVar + ".call(JSON.stringify(parameter), callback || null)); }; ";
+				String callFunctions;
+				
+				if (USE_TO_STRING) {
+					bindings.put(callVar, new FromScriptUsingToString(scriptEngine, executorService, nextCallbackFunctionSuffix, fail, asyncFunction, syncFunction));
+					callFunctions = "var " + CALL_FUNCTION_NAME + " = function(parameter, callback) { return JSON.parse(" + callVar + ".call(JSON.stringify(parameter), callback || null)); }; ";
+				} else {
+					try {
+						scriptEngine.eval("var " + UNICITY_PREFIX + "convertFrom = function(o) {"
+								+ "if (o == null) {"
+									+ "return null;"
+								+ "}"
+								+ "if (o instanceof Array) {"
+									+ "var p = new com.google.gson.JsonArray();"
+									+ "for (k in o) {"
+										+ "p.add(" + UNICITY_PREFIX + "convertFrom(o[k]));"
+									+ "}"
+									+ "return p;"
+								+ "}"
+								+ "if (o instanceof Object) {"
+									+ "var p = new com.google.gson.JsonObject();"
+									+ "for (k in o) {"
+										+ "p.add(k, " + UNICITY_PREFIX + "convertFrom(o[k]));"
+									+ "}"
+									+ "return p;"
+								+ "}"
+								+ "return new com.google.gson.JsonPrimitive(o);"
+							+ "};");
+						scriptEngine.eval("var " + UNICITY_PREFIX + "convertTo = function(o) {"
+								+ "if (o == null) {"
+									+ "return null;"
+								+ "}"
+								+ "if (o.isJsonObject()) {"
+									+ "var i = o.entrySet().iterator();"
+									+ "var p = {};"
+									+ "while (i.hasNext()) {"
+										+ "var e = i.next();"
+										+ "p[e.getKey()] = " + UNICITY_PREFIX + "convertTo(e.getValue());"
+									+ "}"
+									+ "return p;"
+								+ "}"
+								+ "if (o.isJsonPrimitive()) {"
+									+ "var oo = o.getAsJsonPrimitive();"
+									+ "if (oo.isString()) {"
+										+ "return '' + oo.getAsString();" // ['' +] looks to be necessary
+									+ "}"
+									+ "if (oo.isNumber()) {"
+										+ "return oo.getAsNumber();"
+									+ "}"
+									+ "if (oo.isBoolean()) {"
+										+ "return oo.getAsBoolean();"
+									+ "}"
+									+ "return null;"
+								+ "}"
+								+ "return null;"
+							+ "};");
+					} catch (Exception e) {
+						LOGGER.error("Script error", e);
+						if (fail != null) {
+							fail.failed(new IOException(e));
+						}
+						return;
+					}
+
+					bindings.put(callVar, new FromScriptUsingConvert(scriptEngine, executorService, nextCallbackFunctionSuffix, fail, asyncFunction, syncFunction));
+					callFunctions = "var " + CALL_FUNCTION_NAME + " = function(parameter, callback) { return " + UNICITY_PREFIX + "convertTo(" + callVar + ".call(" + UNICITY_PREFIX + "convertFrom(parameter), callback || null)); }; ";
+				}
+
 				
 				try {
 					scriptEngine.eval(ScriptUtils.functions());
@@ -224,4 +312,70 @@ public final class ExecutorScriptRunner implements ScriptRunner<JsonElement>, Au
 			}
 		});
 	}
+	
+	/*%%%%%%%%%%%%%%%%%%%%%%%%%%
+	public static JsonElement fromScript(Object o) {
+		System.out.println(o);
+		return null;
+	}
+	public static void print(JsonElement o) {
+		System.out.println("---> " + o);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+		ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
+		scriptEngine.eval("var foo = function(o) {"
+				+ "if (o instanceof Array) {"
+					+ "var p = new com.google.gson.JsonArray();"
+					+ "for (k in o) {"
+						+ "p.add(foo(o[k]));"
+					+ "}"
+					+ "return p;"
+				+ "}"
+				+ "if (o instanceof Object) {"
+					+ "var p = new com.google.gson.JsonObject();"
+					+ "for (k in o) {"
+						+ "p.add(k, foo(o[k]));"
+					+ "}"
+					+ "return p;"
+				+ "}"
+				+ "return new com.google.gson.JsonPrimitive(o);"
+			+ "};");
+		scriptEngine.eval("var foo2 = function(o) {"
+				+ "if (o.isJsonObject()) {"
+					+ "var i = o.entrySet().iterator();"
+					+ "var p = {};"
+					+ "while (i.hasNext()) {"
+						+ "var e = i.next();"
+						+ "p[e.getKey()] = foo2(e.getValue());"
+					+ "}"
+					+ "return p;"
+				+ "}"
+				+ "if (o.isJsonPrimitive()) {"
+					+ "var oo = o.getAsJsonPrimitive();"
+					+ "if (oo.isString()) {"
+						+ "return oo.getAsString().toString();"
+					+ "}"
+					+ "if (oo.isNumber()) {"
+						+ "return oo.getAsDouble();"
+					+ "}"
+					+ "if (oo.isBoolean()) {"
+						+ "return oo.getAsBoolean();"
+					+ "}"
+					+ "return null;"
+				+ "}"
+				+ "return null;"
+			+ "};");
+		String callFunctions = "Java.type(\"" + ExecutorScriptRunner.class.getName() + "\").fromScript({'a':'aa'});";
+		
+		scriptEngine.eval(ExecutorScriptRunner.class.getName() + ".print(foo({'a':'aa', 'b':{'c':'cc','d':'dd'}}));");
+		scriptEngine.eval(ExecutorScriptRunner.class.getName() + ".print(foo('abc'));");
+		scriptEngine.eval(ExecutorScriptRunner.class.getName() + ".print(foo(678));");
+		scriptEngine.eval(ExecutorScriptRunner.class.getName() + ".print(foo({'a':'aa', 'b':{'c':'cc','d':123}}));");
+		scriptEngine.eval(ExecutorScriptRunner.class.getName() + ".print(foo({'a':'aa', 'b':{'c':'cc','d':[ 'a1', 'a2' ]}}));");
+		scriptEngine.eval(ExecutorScriptRunner.class.getName() + ".print(foo({'a':'aa', 'b':{'c':'cc','d':[ 'a1', 123, 456 ]}}));");
+		scriptEngine.eval("java.lang.System.out.println(JSON.stringify(foo2(foo({'a':'aa', 'b':{'c':'cc','d':'dd',e:4.5,f:true,g:500}}))));");
+	}
+	*/
 }
