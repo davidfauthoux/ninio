3a66ed65d2c2b23e4904ad5cdfaf5d79c20f8c56
David Fauthoux <david.fauthoux@gmail.com>
2016-02-18 12:51:06 +0100

    Fix SNMP v3

--- ninio-snmp/src/main/java/com/davfx/ninio/snmp/Version3PacketParser.java
+++ ninio-snmp/src/main/java/com/davfx/ninio/snmp/Version3PacketParser.java
@@ -12,7 +12,9 @@ final class Version3PacketParser {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Version3PacketParser.class);
 	
-	private static final Oid AUTH_ERROR_OID = new Oid("1.3.6.1.6.3.15.1.1");
+	private static final Oid AUTH_ERROR_OID_PREFIX = new Oid("1.3.6.1.6.3.15.1.1");
+	private static final Oid AUTH_ERROR_UNKNOWN_ENGINE_ID_OID = new Oid("1.3.6.1.6.3.15.1.1.4.0");
+	private static final Oid AUTH_ERROR_NOT_IN_TIME_WINDOW_OID = new Oid("1.3.6.1.6.3.15.1.1.2.0");
 	
 	private final int requestId;
 	private final int errorStatus;
@@ -42,6 +44,9 @@ final class Version3PacketParser {
 			}
 			ber.endReadSequence();
 
+			int previousEngineTime = authEngine.getTime();
+			int previousEngineBootCount = authEngine.getBootCount();
+			
 			BerReader secBer = new BerReader(ber.readBytes());
 			secBer.beginReadSequence();
 			{
@@ -75,105 +80,84 @@ final class Version3PacketParser {
 			pdu.beginReadSequence();
 			pdu.readBytes();
 			pdu.readBytes();
-			
-			if (!authEngine.isReady()) {
-				LOGGER.trace("Engine not ready");
-				if (ber.beginReadSequence() != BerConstants.REPORT) {
-					throw new IOException("Not a report packet");
-				}
-				{
-					requestId = pdu.readInteger();
-					LOGGER.trace("REPORT <- requestId = {}", requestId);
-					{
-						int errorStatus = pdu.readInteger();
-						int errorIndex = pdu.readInteger();
-						LOGGER.trace("REPORT error = {}/{}", errorStatus, errorIndex);
-					}
 
-					errorStatus = BerConstants.ERROR_STATUS_RETRY;
-					errorIndex = 0;
-					LOGGER.trace("REPORT error replaced = {}/{}", errorStatus, errorIndex);
-	
-					pdu.beginReadSequence();
-					{
-						while (pdu.hasRemainingInSequence()) {
-							pdu.beginReadSequence();
-							{
-								Oid oid = pdu.readOid();
-								String value = pdu.readValue();
-								// pdu.readOidValue();
-								LOGGER.trace("<- {} = {}", oid, value);
+			int requestId;
+			int errorStatus;
+			int errorIndex;
+
+			int s = pdu.beginReadSequence();
+			if (s == BerConstants.REPORT) {
+				requestId = pdu.readInteger();
+				LOGGER.trace("REPORT <- requestId = {}", requestId);
+				errorStatus = pdu.readInteger();
+				errorIndex = pdu.readInteger();
+				LOGGER.trace("REPORT error = {}/{}", errorStatus, errorIndex);
+
+				pdu.beginReadSequence();
+				{
+					while (pdu.hasRemainingInSequence()) {
+						pdu.beginReadSequence();
+						{
+							Oid oid = pdu.readOid();
+							String value = pdu.readValue();
+							LOGGER.trace("<- {} = {}", oid, value);
+							if (!authEngine.isReady() && AUTH_ERROR_UNKNOWN_ENGINE_ID_OID.isPrefixOf(oid)) {
+								LOGGER.error("Engine not known ({}), requestId = {}", oid, requestId);
+								errorStatus = BerConstants.ERROR_STATUS_RETRY;
+								errorIndex = 0;
+								requestId = Integer.MAX_VALUE;
+							} else if (authEngine.isReady() && (previousEngineTime == 0) && (previousEngineBootCount == 0) && (authEngine.getTime() > 0) && (authEngine.getBootCount() > 0) && AUTH_ERROR_NOT_IN_TIME_WINDOW_OID.isPrefixOf(oid)) {
+								LOGGER.error("Engine not synced ({}), requestId = {}", oid, requestId);
+								errorStatus = BerConstants.ERROR_STATUS_RETRY;
+								errorIndex = 0;
+								requestId = Integer.MAX_VALUE;
+							} else if (AUTH_ERROR_OID_PREFIX.isPrefixOf(oid)) {
+								LOGGER.error("Authentication failed ({}), requestId = {}", oid, requestId);
+								errorStatus = BerConstants.ERROR_STATUS_AUTHENTICATION_FAILED;
+								errorIndex = 0;
+								requestId = Integer.MAX_VALUE;
 							}
-							pdu.endReadSequence();
+							// OidValue value = pdu.readOidValue();
+							results.add(new Result(oid, value));
 						}
+						pdu.endReadSequence();
 					}
-					pdu.endReadSequence();
 				}
 				pdu.endReadSequence();
 			} else {
-				int s = pdu.beginReadSequence();
-				if (s == BerConstants.REPORT) {
-					requestId = pdu.readInteger();
-					LOGGER.trace("REPORT <- requestId = {}", requestId);
-					int errorStatus = pdu.readInteger();
-					int errorIndex = pdu.readInteger();
-					LOGGER.trace("REPORT error = {}/{}", errorStatus, errorIndex);
+				if (s != BerConstants.RESPONSE) {
+					throw new IOException("Not a response packet");
+				}
+				requestId = pdu.readInteger();
+				LOGGER.trace("RESPONSE <- requestId = {}", requestId);
+				errorStatus = pdu.readInteger();
+				errorIndex = pdu.readInteger();
+				LOGGER.trace("RESPONSE error = {}/{}", errorStatus, errorIndex);
 
-					pdu.beginReadSequence();
-					{
-						while (pdu.hasRemainingInSequence()) {
-							pdu.beginReadSequence();
-							{
-								Oid oid = pdu.readOid();
-								String value = pdu.readValue();
-								LOGGER.trace("<- {} = {}", oid, value);
-								if (AUTH_ERROR_OID.isPrefixOf(oid)) {
-									LOGGER.error("Authentication failed ({}), requestId = {}", oid, requestId);
-									errorStatus = BerConstants.ERROR_STATUS_AUTHENTICATION_FAILED;
-									errorIndex = 0;
-								}
-								// OidValue value = pdu.readOidValue();
-								results.add(new Result(oid, value));
-							}
-							pdu.endReadSequence();
+				pdu.beginReadSequence();
+				{
+					while (pdu.hasRemainingInSequence()) {
+						pdu.beginReadSequence();
+						{
+							Oid oid = pdu.readOid();
+							String value = pdu.readValue();
+							LOGGER.trace("<- {} = {}", oid, value);
+							// OidValue value = pdu.readOidValue();
+							results.add(new Result(oid, value));
 						}
+						pdu.endReadSequence();
 					}
-					pdu.endReadSequence();
-					
-					this.errorStatus = errorStatus;
-					this.errorIndex = errorIndex;
-				} else {
-					if (s != BerConstants.RESPONSE) {
-						throw new IOException("Not a response packet");
-					}
-					requestId = pdu.readInteger();
-					LOGGER.trace("RESPONSE <- requestId = {}", requestId);
-					errorStatus = pdu.readInteger();
-					errorIndex = pdu.readInteger();
-					LOGGER.trace("RESPONSE error = {}/{}", errorStatus, errorIndex);
-
-					pdu.beginReadSequence();
-					{
-						while (pdu.hasRemainingInSequence()) {
-							pdu.beginReadSequence();
-							{
-								Oid oid = pdu.readOid();
-								String value = pdu.readValue();
-								LOGGER.trace("<- {} = {}", oid, value);
-								// OidValue value = pdu.readOidValue();
-								results.add(new Result(oid, value));
-							}
-							pdu.endReadSequence();
-						}
-					}
-					pdu.endReadSequence();
 				}
 				pdu.endReadSequence();
 			}
 			pdu.endReadSequence();
+
+			this.requestId = requestId;
+			this.errorStatus = errorStatus;
+			this.errorIndex = errorIndex;
 		}
 		ber.endReadSequence();
-		
+
 		authEngine.setReady();
 	}
 
