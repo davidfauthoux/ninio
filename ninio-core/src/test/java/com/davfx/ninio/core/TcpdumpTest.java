85c462847a564abd20e5c8aaa12b0d150de12d1e
David Fauthoux <david.fauthoux@gmail.com>
2016-03-02 14:39:54 +0100

    Ignoring too much machine-dependent test

--- ninio-core/src/test/java/com/davfx/ninio/core/TcpdumpTest.java
+++ ninio-core/src/test/java/com/davfx/ninio/core/TcpdumpTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +19,7 @@ public class TcpdumpTest {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(TcpdumpTest.class);
 	
+	@Ignore
 	@Test
 	public void test() throws Exception {
 		System.setProperty("ninio.tcpdump.mode", "hex"); // raw not working on Mac OS X
