17fbbe4a1ee89125efe93a133202367aa12512a8
David Fauthoux <david.fauthoux@gmail.com>
2016-03-31 17:03:14 +0200

    Accepting any certificate

--- src/main/java/com/davfx/ninio/common/Trust.java
+++ src/main/java/com/davfx/ninio/common/Trust.java
@@ -5,11 +5,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
+import java.security.cert.X509Certificate;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
+import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 public final class Trust {
 	private final SSLContext sslContext;
@@ -72,12 +75,66 @@ public final class Trust {
 	
 	public Trust() throws IOException {
 		try {
+			/*%%
+			KeyStore ksTrust = KeyStore.getInstance("JKS");
+
+			TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+			tmf.init(ksTrust);
+
+			TrustManager defaultTrustManager = new SavingTrustManager((X509TrustManager) tmf.getTrustManagers()[0]);
+			
+			TrustManager[] t = new TrustManager[] { defaultTrustManager }
+			*/
+			
+			TrustManager[] t = new TrustManager[] {
+				new X509TrustManager() {
+					@Override
+					public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+						return null;
+					}
+					@Override
+					public void checkClientTrusted(X509Certificate[] certs, String authType) {
+					}
+					@Override
+					public void checkServerTrusted(X509Certificate[] certs, String authType) {
+					}
+				}
+			};
+			
 			sslContext = SSLContext.getInstance("TLS");
-			sslContext.init(null, null, null);
+			sslContext.init(null, t, null);
 		} catch (Exception e) {
 			throw new IOException(e);
 		}
 	}
+	
+	/*%%
+	private static class SavingTrustManager implements X509TrustManager {
+
+		private final X509TrustManager tm;
+		private X509Certificate[] chain;
+
+		SavingTrustManager(X509TrustManager tm) {
+			this.tm = tm;
+		}
+
+		@Override
+		public X509Certificate[] getAcceptedIssuers() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			this.chain = chain;
+			tm.checkServerTrusted(chain, authType);
+		}
+	}
+	*/
 	
 	public SSLEngine createEngine(boolean clientMode) {
 		SSLEngine engine = sslContext.createSSLEngine();
