9557669eef66e7cead8a042d863c4589c7aa9574
David Fauthoux <david.fauthoux@gmail.com>
2016-03-31 17:17:27 +0200

    Accepting any certificate

--- ninio-core/src/main/java/com/davfx/ninio/core/Trust.java
+++ ninio-core/src/main/java/com/davfx/ninio/core/Trust.java
@@ -6,12 +6,15 @@ import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.cert.X509Certificate;
 import java.util.Enumeration;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
+import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -92,12 +95,67 @@ public final class Trust {
 	public Trust() {
 		ksKeys = null;
 		try {
+			/*%%
+			KeyStore ksTrust = KeyStore.getInstance("JKS");
+
+			TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+			tmf.init(ksTrust);
+
+			TrustManager defaultTrustManager = new SavingTrustManager((X509TrustManager) tmf.getTrustManagers()[0]);
+			
+			TrustManager[] t = new TrustManager[] { defaultTrustManager }
+			*/
+			
+			// WARNING! Not secure to a man-in-the-middle attack
+			TrustManager[] t = new TrustManager[] {
+				new X509TrustManager() {
+					@Override
+					public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+						return null;
+					}
+					@Override
+					public void checkClientTrusted(X509Certificate[] certs, String authType) {
+					}
+					@Override
+					public void checkServerTrusted(X509Certificate[] certs, String authType) {
+					}
+				}
+			};
+			
 			sslContext = SSLContext.getInstance("TLS");
-			sslContext.init(null, null, null);
+			sslContext.init(null, t, null);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	/*%%
+	private static class SavingTrustManager implements X509TrustManager {
+
+		private final X509TrustManager tm;
+		private X509Certificate[] chain;
+
+		SavingTrustManager(X509TrustManager tm) {
+			this.tm = tm;
+		}
+
+		@Override
+		public X509Certificate[] getAcceptedIssuers() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			this.chain = chain;
+			tm.checkServerTrusted(chain, authType);
+		}
+	}
+	*/
 	
 	public PrivateKey getPrivateKey(String alias, String password) {
 		try {
