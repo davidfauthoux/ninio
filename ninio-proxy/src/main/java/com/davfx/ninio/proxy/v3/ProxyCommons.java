8e4e6e09b4bc9eb5ebd6222154998b31c20b8fe5
David Fauthoux <david.fauthoux@gmail.com>
2016-04-27 16:23:02 +0200

    Proxy

---
+++ ninio-proxy/src/main/java/com/davfx/ninio/proxy/v3/ProxyCommons.java
@@ -0,0 +1,28 @@
+package com.davfx.ninio.proxy.v3;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+interface ProxyCommons {
+	interface Commands {
+		int SEND_WITH_ADDRESS = 0;
+		int SEND_WITHOUT_ADDRESS = 1;
+		int CLOSE = 2;
+		int CONNECT_WITH_ADDRESS = 3;
+		int CONNECT_WITHOUT_ADDRESS = 4;
+	}
+	
+	final class Types {
+		
+		private static final Config CONFIG = ConfigFactory.load(ProxyCommons.class.getClassLoader());
+		
+		public static final String TCP = CONFIG.getString("ninio.proxy.tcp");
+		public static final String SSL = CONFIG.getString("ninio.proxy.ssl");
+		public static final String UDP = CONFIG.getString("ninio.proxy.udp");
+		public static final String PING = CONFIG.getString("ninio.proxy.ping");
+		public static final String HOP = CONFIG.getString("ninio.proxy.hop");
+		
+		private Types() {
+		}
+	}
+}
