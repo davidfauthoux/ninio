80dc72bf2e2afcd0acc5a3f462445e05ac69af1a
David Fauthoux <david.fauthoux@gmail.com>
2017-01-26 11:49:29 +0100

    Threading available (if > 1)

--- ninio-core/src/main/java/com/davfx/ninio/core/Ninio.java
+++ ninio-core/src/main/java/com/davfx/ninio/core/Ninio.java
@@ -1,14 +1,26 @@
 package com.davfx.ninio.core;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicLong;
 
+import com.davfx.ninio.core.dependencies.Dependencies;
+import com.davfx.ninio.util.ConfigUtils;
 import com.davfx.ninio.util.SerialExecutor;
+import com.typesafe.config.Config;
 
 public final class Ninio implements AutoCloseable {
 	private final InternalQueue internalQueue = new InternalQueue();
-	private final SerialExecutor internalExecutor = new SerialExecutor(Ninio.class); // Only one thread for now
+
+	private static final Config CONFIG = ConfigUtils.load(new Dependencies()).getConfig(Ninio.class.getPackage().getName());
+	private static final int THREADING = CONFIG.getInt("threading");
+
+	private final SerialExecutor[] internalExecutors = new SerialExecutor[THREADING];
+	private final AtomicLong internalExecutorIndex = new AtomicLong(0L);
 	
 	private Ninio() {
+		for (int i = 0; i < THREADING; i++) {
+			internalExecutors[i] = new SerialExecutor(Ninio.class);
+		}
 	}
 	
 	@Override
@@ -28,7 +40,7 @@ public final class Ninio implements AutoCloseable {
 			}
 			@Override
 			public Executor executor() {
-				return internalExecutor;
+				return internalExecutors[(internalExecutors.length == 1) ? 0 : ((int) (internalExecutorIndex.getAndIncrement() % internalExecutors.length))];
 			}
 		});
 	}
