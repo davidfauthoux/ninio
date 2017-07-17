d170d5a2691e6dc4818a14f6723f042b60c4a068
David Fauthoux <david.fauthoux@gmail.com>
2017-07-17 17:09:09 +0200

    Bug fix CSV delimiter at end of line after a quote

--- ninio-csv/src/main/java/com/davfx/ninio/csv/CsvRead.java
+++ ninio-csv/src/main/java/com/davfx/ninio/csv/CsvRead.java
@@ -181,65 +181,58 @@ public final class CsvRead {
 		
 		@Override
 		public Iterable<String> next() throws IOException {
+			boolean inQuote = false;
+			StringBuilder c = new StringBuilder();
+			List<String> components = new LinkedList<String>();
+
 			while (true) {
 				String line = reader.readLine();
 				if (line == null) {
 					return null;
 				}
 				
-				line = line.trim();
-				if (ignoreEmptyLines && line.isEmpty()) {
-					continue;
-				}
-
-				List<String> components = new LinkedList<String>();
-				
-				int i = 0;
-				while (i < line.length()) {
-					if (line.charAt(i) == quote) {
-						StringBuilder c = new StringBuilder();
-						int n = i + 1;
-						while (true) {
-							int j = line.indexOf(quote, n);
-							if (j < 0) {
-								String component = line.substring(n);
-								c.append(component).append('\n');
-								line = reader.readLine();
-								n = 0;
-								continue;
-							}
-							if ((j < (line.length() - 1)) && (line.charAt(j + 1) == quote)) {
-								String component = line.substring(n, j);
-								c.append(component).append(quote);
-								n = j + 2;
-								continue;
-							} else {
-								String component = line.substring(n, j);
-								c.append(component);
-								components.add(c.toString());
-								// Next char is ';'
-								i = j + 2;
-								break;
-							}
-						}
-					} else {
-						int j = line.indexOf(delimiter, i);
-						if (j < 0) {
-							String component = line.substring(i);
-							components.add(component);
-							i = line.length();
-						} else {
-							String component = line.substring(i, j);
-							components.add(component);
-							i = j + 1;
-							if (i == line.length()) {
-								components.add("");
-							}
-						}
+				if (!inQuote) {
+					line = line.trim();
+					if (ignoreEmptyLines && line.isEmpty()) {
+						continue;
 					}
 				}
-				return components;
+
+				
+				char last = delimiter;
+				int i = 0;
+				while (i < line.length()) {
+					char character = line.charAt(i);
+					if (inQuote) {
+						if (character == quote) {
+							if (last == quote) {
+								c.append(quote);
+							}
+							inQuote = false;
+						} else {
+							c.append(character);
+						}
+						last = character;
+					} else {
+						if (character == quote) {
+							inQuote = true;
+						} else if (character == delimiter) {
+							components.add(c.toString());
+							c.setLength(0);
+						} else {
+							c.append(character);
+						}
+					}
+					i++;
+				}
+				
+				if (!inQuote) {
+					components.add(c.toString());
+					break;
+				}
 			}
+
+			return components;
 		}
 	}
 	
