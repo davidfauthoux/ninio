4dfc05b68e727f312b03d27282a79b387840ce2e
David Fauthoux <david.fauthoux@gmail.com>
2019-02-05 14:36:15 +0200

    Adding PingTimeout.ping to be able to provide the Timeout instance

--- ninio-ping/src/main/java/com/davfx/ninio/ping/PingTimeout.java
+++ ninio-ping/src/main/java/com/davfx/ninio/ping/PingTimeout.java
@@ -23,37 +23,41 @@ public final class PingTimeout {
 			}
 			
 			@Override
-			public Cancelable ping(byte[] ip, final PingReceiver receiver) {
-				final Timeout.Manager m = t.set(timeout);
+			public Cancelable ping(byte[] ip, PingReceiver receiver) {
+				return PingTimeout.ping(t, timeout, ip, wrappee, receiver);
+			}
+		};
+	}
+	
+	public static Cancelable ping(Timeout t, double timeout, byte[] ip, PingConnecter wrappee, final PingReceiver receiver) {
+		final Timeout.Manager m = t.set(timeout);
 
-				final Cancelable cancelable = wrappee.ping(ip, new PingReceiver() {
-					@Override
-					public void received(double time) {
-						m.cancel();
-						receiver.received(time);
-					}
-					@Override
-					public void failed(IOException ioe) {
-						m.cancel();
-						receiver.failed(ioe);
-					}
-				});
-				
-				m.run(new Runnable() {
-					@Override
-					public void run() {
-						cancelable.cancel();
-						receiver.failed(new IOException("Timeout"));
-					}
-				});
+		final Cancelable cancelable = wrappee.ping(ip, new PingReceiver() {
+			@Override
+			public void received(double time) {
+				m.cancel();
+				receiver.received(time);
+			}
+			@Override
+			public void failed(IOException ioe) {
+				m.cancel();
+				receiver.failed(ioe);
+			}
+		});
+		
+		m.run(new Runnable() {
+			@Override
+			public void run() {
+				cancelable.cancel();
+				receiver.failed(new IOException("Timeout"));
+			}
+		});
 
-				return new Cancelable() {
-					@Override
-					public void cancel() {
-						m.cancel();
-						cancelable.cancel();
-					}
-				};
+		return new Cancelable() {
+			@Override
+			public void cancel() {
+				m.cancel();
+				cancelable.cancel();
 			}
 		};
 	}
