e941be7e27d5ec010da4a416614daf7da14d975c
David Fauthoux <david.fauthoux@gmail.com>
2019-03-06 13:10:22 +0200

    Some tests and required pom.xml (including ninio-string)

---
+++ ninio-ping/src/test/java/com/davfx/ninio/ping/DoubleClientPingTest.java
@@ -0,0 +1,87 @@
+package com.davfx.ninio.ping;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.davfx.ninio.core.Address;
+import com.davfx.ninio.core.Ninio;
+import com.davfx.ninio.util.Lock;
+
+//mvn install dependency:copy-dependencies
+//sudo java -cp target/dependency/*:target/test-classes/:target/classes/ com.davfx.ninio.ping.PingTest
+public class DoubleClientPingTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DoubleClientPingTest.class);
+
+	public static void main(String[] args) throws Exception {
+		byte[] pingHost0 = InetAddress.getByName("37.187.120.93").getAddress();
+		byte[] pingHost1 = InetAddress.getByName("8.8.8.8").getAddress();
+		// new byte[] { 8, 8, 8, 8 };
+		// ::1
+
+		try (Ninio ninio = Ninio.create()) {
+			final List<Lock<Double, IOException>> locks = new LinkedList<Lock<Double, IOException>>();
+			for (int i = 0; i < 10; i++) {
+				locks.add(new Lock<Double, IOException>());
+			}
+			
+			try (PingConnecter client0 = PingTimeout.wrap(10d, ninio.create(PingClient.builder()))) {
+				client0.connect(new PingConnection() {
+					@Override
+					public void failed(IOException ioe) {
+						for (Lock<Double, IOException> lock : locks) {
+							lock.fail(ioe);
+						}
+					}
+					@Override
+					public void connected(Address address) {
+					}
+					@Override
+					public void closed() {
+					}
+				});
+				try (PingConnecter client1 = PingTimeout.wrap(10d, ninio.create(PingClient.builder()))) {
+					client1.connect(new PingConnection() {
+						@Override
+						public void failed(IOException ioe) {
+							for (Lock<Double, IOException> lock : locks) {
+								lock.fail(ioe);
+							}
+						}
+						@Override
+						public void connected(Address address) {
+						}
+						@Override
+						public void closed() {
+						}
+					});
+					boolean ping0 = true;
+					for (final Lock<Double, IOException> lock : locks) {
+						final boolean p = ping0;
+						(ping0 ? client0 : client1).ping(ping0 ? pingHost1 : pingHost0, new PingReceiver() {
+							@Override
+							public void received(double time) {
+								LOGGER.debug("From client {}: {}", (p ? "0":"1"), time);
+								lock.set(time);
+							}
+							@Override
+							public void failed(IOException ioe) {
+								lock.fail(ioe);
+							}
+						});
+						ping0 = !ping0;
+					}
+					
+					for (Lock<Double, IOException> lock : locks) {
+						System.out.println(lock.waitFor());
+					}
+				}
+			}
+		}
+	}
+
+}
---
+++ ninio-proxy/src/test/java/com/davfx/ninio/proxy/ProxyOpenCloseOpenPingTest.java
@@ -0,0 +1,89 @@
+package com.davfx.ninio.proxy;
+
+import java.io.IOException;
+
+import com.davfx.ninio.core.Address;
+import com.davfx.ninio.core.Disconnectable;
+import com.davfx.ninio.core.Ninio;
+import com.davfx.ninio.ping.PingClient;
+import com.davfx.ninio.ping.PingConnecter;
+import com.davfx.ninio.ping.PingConnection;
+import com.davfx.ninio.ping.PingReceiver;
+import com.davfx.ninio.ping.PingTimeout;
+import com.davfx.ninio.util.Lock;
+
+//mvn install dependency:copy-dependencies
+//sudo java -cp target/dependency/*:target/test-classes/:target/classes/ com.davfx.ninio.proxy.PingTest
+public class ProxyOpenCloseOpenPingTest {
+
+	public static void main(String[] args) throws Exception {
+		byte[] pingHost = new byte[] { 8, 8, 8, 8 };
+		// ::1
+
+		int proxyPort = 8081;
+		
+		try (Ninio ninio = Ninio.create()) {
+			try (Disconnectable proxyServer = ninio.create(ProxyServer.defaultServer(new Address(Address.ANY, proxyPort), null))) {
+				try (ProxyProvider proxyClient0 = ninio.create(ProxyClient.defaultClient(new Address(Address.LOCALHOST, proxyPort)))) {
+					final Lock<Double, IOException> lock0 = new Lock<>();
+					try (PingConnecter client0 = PingTimeout.wrap(1d, ninio.create(PingClient.builder().with(proxyClient0.raw())))) {
+						client0.connect(new PingConnection() {
+							@Override
+							public void failed(IOException ioe) {
+								lock0.fail(ioe);
+							}
+							@Override
+							public void connected(Address address) {
+							}
+							@Override
+							public void closed() {
+							}
+						});
+						client0.ping(pingHost, new PingReceiver() {
+							@Override
+							public void received(double time) {
+								lock0.set(time);
+							}
+							@Override
+							public void failed(IOException ioe) {
+								lock0.fail(ioe);
+							}
+						});
+						
+						System.out.println(lock0.waitFor());
+					}
+				}
+				try (ProxyProvider proxyClient1 = ninio.create(ProxyClient.defaultClient(new Address(Address.LOCALHOST, proxyPort)))) {
+					final Lock<Double, IOException> lock1 = new Lock<>();
+					try (PingConnecter client1 = PingTimeout.wrap(1d, ninio.create(PingClient.builder().with(proxyClient1.raw())))) {
+						client1.connect(new PingConnection() {
+							@Override
+							public void failed(IOException ioe) {
+								lock1.fail(ioe);
+							}
+							@Override
+							public void connected(Address address) {
+							}
+							@Override
+							public void closed() {
+							}
+						});
+						client1.ping(pingHost, new PingReceiver() {
+							@Override
+							public void received(double time) {
+								lock1.set(time);
+							}
+							@Override
+							public void failed(IOException ioe) {
+								lock1.fail(ioe);
+							}
+						});
+						
+						System.out.println(lock1.waitFor());
+					}
+				}
+			}
+		}
+	}
+	
+}
