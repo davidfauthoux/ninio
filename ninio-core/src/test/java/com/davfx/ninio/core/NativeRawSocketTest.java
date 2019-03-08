38559a9548b5adedb7360ea9eb2f9e8c84b9a537
David Fauthoux <david.fauthoux@gmail.com>
2019-03-08 10:34:04 +0200

    Testing NativeRawSocket

---
+++ ninio-core/src/test/java/com/davfx/ninio/core/NativeRawSocketTest.java
@@ -0,0 +1,79 @@
+package com.davfx.ninio.core;
+
+import java.nio.ByteBuffer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//mvn install dependency:copy-dependencies
+//sudo java -cp target/dependency/*:target/test-classes/:target/classes/ com.davfx.ninio.core.NativeRawSocketTest
+public class NativeRawSocketTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(NativeRawSocketTest.class);
+
+	public static void main(String[] args) throws Exception {
+		byte[] pingHost = new byte[] { 8, 8, 8, 8 };
+		// ::1
+		int id = 1234;
+
+		try (Ninio ninio = Ninio.create()) {
+			NativeRawSocket s = new NativeRawSocket(NativeRawSocket.PF_INET, 1);
+
+			byte[] sendData = new byte[16];
+
+			ByteBuffer b = ByteBuffer.wrap(sendData);
+			b.put((byte) 8); // requestType (Echo)
+			b.put((byte) 0); // code
+			int checksumPosition = b.position();
+			b.putShort((short) 0); // checksum
+			b.putShort((short) ((id >>> 16) & 0xFFFF)); // identifier
+			b.putShort((short) (id & 0xFFFF)); // sequence
+			long nt = System.nanoTime();
+			b.putLong(nt);
+			int endPosition = b.position();
+
+			b.position(0);
+			int checksum = 0;
+			while (b.position() < endPosition) {
+				checksum += b.getShort() & 0xFFFF;
+			}
+		    while((checksum & 0xFFFF0000) != 0) {
+		    	checksum = (checksum & 0xFFFF) + (checksum >>> 16);
+		    }
+
+		    checksum = (~checksum & 0xffff);
+			b.position(checksumPosition);
+			b.putShort((short) (checksum & 0xFFFF));
+			b.position(endPosition);
+			b.flip();
+			
+			s.write(pingHost, b.array(), 0, b.capacity());
+			
+			byte[] recvData = new byte[84];
+			byte[] srcAddress = new byte[4];
+			
+			s.close();
+
+			
+			int r = s.read(recvData, 0, recvData.length, srcAddress);
+			Address a = new Address(srcAddress, 0);
+			LOGGER.debug("Received raw packet: {} bytes from: {}", r, a);
+
+			ByteBuffer bb = ByteBuffer.wrap(recvData, 0, r);
+			int headerLength = (bb.get() & 0x0F) * 4;
+			bb.position(headerLength);
+			
+			long now = System.nanoTime();
+			int type = bb.get() & 0xFF; // type
+			int code = bb.get() & 0xFF; // code
+			bb.getShort(); // checksum
+			short identifier = bb.getShort(); // identifier
+			short sequence = bb.getShort(); // sequence
+			long time = bb.getLong();
+			id = (int) (((identifier & 0xFFFFL) << 16) | (sequence & 0xFFFFL));
+
+			long deltaNano = now - time;
+			LOGGER.info("Received ICMP packet [{}/{}] (ID {}): {} ns", type, code, id, deltaNano);
+			
+			s.close();
+		}
+	}}
